dc23cc5d4b96fe2b3efa2aefd8e22a76eb3b255d
armando <armando.lezama.psic@gmail.com>
2021-03-13 17:56:46 -0600

    chore(*):added admin nav bar and mocks relocated

--- src/graficarte-store-app.js
+++ src/graficarte-store-app.js
@@ -1,11 +1,12 @@
 import { LitElement, html } from 'lit-element';
 import styles from './graficarte-store-app-styles';
 import 'sophos-simple-template/sophos-simple-template';
-import productMocks from './products-mocks';
-import inventoryMocks from './inventory-mocks'
+import productMocks from './mocks/products-mocks';
+import inventoryMocks from './mocks/inventory-mocks'
 import './pages/graficarte-inventory-page'
 import './pages/graficarte-store-home-page';
 import './pages/graficarte-login-page';
+import './nav-bar/graficarte-store-admin-nav-bar'
 export class GraficarteStoreApp extends LitElement {
   /**
     * Instance of the element is created/upgraded. Useful for initializing
@@ -45,41 +46,58 @@ export class GraficarteStoreApp extends LitElement {
     return html`
       <div id="main-app-container">
         ${this.page === 'inventory' ? html`
+          
           <sophos-simple-template
             id="admin-inventory-container"
             styleTemplate="full-nav"
             page-name="${this.page}">
+
             <graficarte-inventory-page 
               .products="${this.inventory}" 
               slot="main-view-content">
             </graficarte-inventory-page>
+
+            <graficarte-store-admin-nav-bar
+              slot="nav-bar-content">
+            </graficarte-store-admin-nav-bar>
+
           </sophos-simple-template>
+    
         ` : html``}
         ${this.page === 'store' ? html`
+
           <sophos-simple-template 
             id="public-store-container"
             page-name="${this.page}">
+
               <div 
                 id="search-bar" 
                 slot="header-content">
+
                 <input 
                   id="search-bar-input" 
                   type="text" name="search-bar" 
                   placeholder="buscar" 
                   @input="${this.searchProduct}">
+
               </div>
+
               <graficarte-store-home-page 
               .products="${this.storeProducts}" 
               slot="main-view-content">
               </graficarte-store-home-page>
+
           </sophos-simple-template>
         ` : html``}
         ${this.page === 'login' ? html`
+
           <graficarte-login-page
-          @graficarte-login-submit="${this.loginSubmit}"></graficarte-login-page>
-        ` : html``}
+           @graficarte-login-submit="${this.loginSubmit}">
+           </graficarte-login-page>
+           
+    ` : html``}
       </div>
-    `;
-  }
-}
+  `;
+  };
+};
 customElements.define('graficarte-store-app', GraficarteStoreApp);
---
+++ src/nav-bar/graficarte-store-admin-nav-bar.js
@@ -0,0 +1,57 @@
+import { LitElement, html, css } from 'lit-element';
+import 'sophos-icon/sophos-icon';
+
+export class GraficarteStoreAdminNavBar extends LitElement {
+  /**
+    * Instance of the element is created/upgraded. Useful for initializing
+    * state, set up event listeners, create shadow dom.
+    * @constructor
+    */
+  constructor() {
+    super();
+  };
+
+  /**
+    * Declared properties and their corresponding attributes
+    */
+  static get properties() {
+    return {
+    };
+  };
+
+  static get styles() {
+    return css`
+      sophos-icon{
+        --sophos-icon-icon-image-width: 100px;
+        --sophos-icon-icon-image-height: 100px;
+        --sophos-icon-icon-text-font-size: 20px;
+      }
+
+      p {
+        font-size: 20px;
+      }
+    `;
+  };
+
+  render() {
+    return html`
+      <div id="admin-nav-bar-container">
+        <sophos-icon
+        imageSource="./assets/admin-user.png"
+        imageAlt="admin-pickture"
+        iconText="Admin-user"
+        iconDirection="top">
+        </sophos-icon>
+        <div>
+          <p>Contabilidad</p>
+          <p>Envíos</p>
+          <p>Inventario</p>
+          <p>Repartidores</p>
+          <p>Historial de ventas</p>
+        </div>
+      </div>
+    
+    `;
+  };
+};
+customElements.define('graficarte-store-admin-nav-bar', GraficarteStoreAdminNavBar);
