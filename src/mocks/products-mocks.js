3859afa40f95498489dfb5df066dd7cf72959654
armandolezama <armando.lezama.psic@gmail.com>
2022-09-10 02:51:00 -0500

    feat: added seed

--- src/mocks/products-mocks.js
+++ src/mocks/products-mocks.js
@@ -63,3 +63,37 @@ export default [
     desctiption : 'Pieza de arte abstracro elaborada en paint. Podrá adornar tu sala'
   }
 ]
+
+
+/*
+This is a seed used in: https://json-generator.com/
+
+
+[
+  '{{repeat(50)}}',
+  {
+    name: function(tags){
+      var typeOfArtWork = [ 'painting', 'sculpture', 'musical recording', 'poem', 'book', 'manuscript', 'statue', 'drawing', 'script'];
+      var featuresOfartWork = ['unique', 'high quality', 'lost', 'common', 'bad quality', 'most popular'];
+      return tags.surname() + ' ' + featuresOfartWork[tags.integer(0, featuresOfartWork.length - 1)]  + ' ' + typeOfArtWork[tags.integer(0, typeOfArtWork.length - 1)];
+    },
+    price: '{{integer(400, 50000, `$0,0.00`)}}',
+    description: '{{lorem(3, "paragraph")}}',
+    ratings: '{{integer(0, 5)}}',
+    active: true,
+    author: '{{firstName()}} {{firstName()}} {{surname()}} {{surname()}}',
+    created: '{{date(new Date(1800, 0, 1), new Date(2000, 0, 1), "YYYY-MM-dd")}}',
+    stock: '{{integer(80, 400)}}',
+    comments: [
+      '{{repeat(3, 10)}}',
+      {
+      name: '{{firstName()}} {{surname()}}',
+      opinion: '{{lorem(2)}}',
+      rating: '{{integer(0, 5)}}',
+      created: '{{date(new Date(2001, 0, 1), new Date(), "YYYY-MM-dd")}}'
+      }
+    ]
+    
+  }
+]
+*/
